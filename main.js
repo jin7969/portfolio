36bbd386326da1e2a92772efcee702b84e03b51e
Ahn <82227098+jin7969@users.noreply.github.com>
2021-08-11 09:05:46 +0900

    js : navbar_menu+contact_btn

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-const navbar = document.querySelector("#navbar");
-const navbarHeight = navbar.getBoundingClientRect().height;
+'use strict';
 
-document.addEventListener("scroll", () => {
+// navbar 상단고정
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark")
+    navbar.classList.remove('navbar--dark');
   }
 });
+
+// navbar menu로 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// contact button 이동
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
